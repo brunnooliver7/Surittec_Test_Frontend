c7942a21d64888a5b1100f5949c847b84e63f1fe
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-09-11 10:01:19 -0300

    formatação NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to="/" className="navbar-brand">Clientes App</Link>
-          <div className="collpase nav-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/list" className="nav-link">Clientes</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/create" className="nav-link">Criar Cliente</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/login" className="nav-link">Login</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+const NavBar = () => (
+  <div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">Clientes App</Link>
+      <div className="collpase nav-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/list" className="nav-link">Clientes</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/create" className="nav-link">Criar Cliente</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/login" className="nav-link">Login</Link>
+          </li>
+        </ul>
       </div>
-    )
-  }
-}
-  
+    </nav>
+  </div>
+);
+
+export default NavBar;
